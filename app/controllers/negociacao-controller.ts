398bffcb4b9477f484b4a5de335a3c94c937c5fe
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-12-17 15:50:44 -0300

    Ajustando as tipagens dos htmlinput

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -6,20 +6,23 @@ import { DiasDaSemana} from "../enums/dia-da-semana.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
-    private inputQuantidade: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement ;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView', false);
 
     constructor(){
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = document.querySelector('#data') as HTMLInputElement;
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
 
     public adiciona(): void {
+        /*
+        comentário exemplo,ignorar
+        */
         const negociacao = Negociacao.criaDe(
             this.inputData.value,
             this.inputQuantidade.value,
